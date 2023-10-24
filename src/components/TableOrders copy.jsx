2274e67c37722b589068662039f5f636efc17842
yair-roshal <yair7705778@gmail.com>
2023-10-24 17:26:29 +0300

    build(deps): update package-lock.json, package.json, public/index.html and TableOrders.jsx

--- src/components/TableOrders copy.jsx
+++ src/components/TableOrders copy.jsx
@@ -10,34 +10,35 @@ import {
   TableBody,
   Button,
   CircularProgress,
-  Paper,
-  Typography,
+  Paper,  Typography,
+
 } from '@mui/material'
-import { styled } from '@mui/material/styles'
+ import { styled } from '@mui/material/styles'
 import { initialState } from '../data/orders.js'
 
 const StyledTableContainer = styled(TableContainer)`
   margin-top: 20px;
-`
+`;
 
 const StyledTable = styled(Table)`
-  min-width: 650px;
-  border-collapse: collapse;
-`
+  min-width: 650px;   border-collapse: collapse; // Слияние границ ячеек для линий между ними
+
+`;
 
 const StyledTableCell = styled(TableCell)`
-  background-color: #f5f5f5;
-  font-weight: bold;
-  border: 1px solid #ddd;
-  padding: 10px;
-`
+  background-color: #f5f5f5; // Цвет фона для ячеек
+  font-weight: bold; // Полужирный текст
+  border: 1px solid #ddd; // Разделительные линии
+  padding: 10px; // Отступы внутри ячейки
+`;
 
 const StyledTableRow = styled(TableRow)`
   &:nth-of-type(odd) {
-    background-color: #e0e0e0;
-    border: 1px solid #ddd;
+    background-color: #e0e0e0; // Цвет фона для четных строк
+    border: 1px solid #ddd; // Разделительные линии
+
   }
-`
+`;
 
 export function TableOrders() {
   const [orders, setOrders] = useState(initialState)
@@ -46,6 +47,8 @@ export function TableOrders() {
   const fetchData = async () => {
     try {
       const response = await axios.get('https://burgerim.ru/orders')
+      console.log('response.data--->', response.data)
+      // Отсортируйте заказы в обратном порядке перед установкой состояния
       setOrders(response.data.reverse())
       setLoading(false)
     } catch (error) {
@@ -67,17 +70,18 @@ export function TableOrders() {
     }
   }
 
+  // Функция для определения цвета строки на основе времени заказа
   const getRowColor = (orderDate) => {
     const currentTime = new Date()
     const orderTime = new Date(orderDate)
-    const timeDifference = (currentTime - orderTime) / (1000 * 60)
+    const timeDifference = (currentTime - orderTime) / (1000 * 60) // Разница в минутах
 
     if (timeDifference <= 5) {
-      return 'lightGreen'
+      return 'lightgreen' // Светло зеленый
     } else if (timeDifference <= 10) {
-      return 'Yellow'
+      return 'lightyellow' // Светло желтый
     } else {
-      return 'lightPink'
+      return 'lightpink' // Светло розовый
     }
   }
 
@@ -99,75 +103,36 @@ export function TableOrders() {
 
   return (
     <Box sx={{ display: 'flex', justifyContent: 'center', width: '100%' }}>
-      <Box p={2} sx={{ width: '100%' }}>
-        <Typography variant='h4' align='center' mb={2}>
-          Orders Dashboard !!!
-        </Typography>
+    <Box p={2} sx={{ width: '100%' }}>
+      
+      
+      
+      <Typography variant="h4" align="center" mb={2}>
+        Orders Dashboard !!!
+      </Typography>
 
-        <Box sx={{ marginTop: '20px', textAlign: 'center' }}>
-           <Box sx={{ display: 'flex', justifyContent: 'space-around' }}>
-            <Box sx={{ display: 'flex', alignItems: 'center' }}>
-              <Box
-                sx={{
-                  backgroundColor: 'lightGreen',
-                  width: '200px',
-                  height: '20px',
-                  marginRight: '5px',
-                }}
-              >
-                менее 5 минут назад
-              </Box>
-            </Box>
-            <Box sx={{ display: 'flex', alignItems: 'center' }}>
-              <Box
-                sx={{
-                  backgroundColor: 'Yellow',
-                  width: '200px',
-                  height: '20px',
-                  marginRight: '5px',
-                }}
-              >
-                5-10 минут назад
-              </Box>
-            </Box>
-            <Box sx={{ display: 'flex', alignItems: 'center' }}>
-              <Box
-                sx={{
-                  backgroundColor: 'lightPink',
-                  width: '200px',
-                  height: '20px',
-                  marginRight: '5px',
-                }}
-              >
-                более 10 минут назад{' '}
-              </Box>
-            </Box>
-          </Box>
-        </Box>
 
-        <StyledTableContainer component={Paper}>
-          <StyledTable aria-label='simple table'>
-            <TableHead>
-              <StyledTableRow>
-                <StyledTableCell align='center'>ID</StyledTableCell>
-                {/* <StyledTableCell align='center'>Query ID</StyledTableCell> */}
-                <StyledTableCell align='center'>Comment</StyledTableCell>
-                <StyledTableCell align='center'>Total Price</StyledTableCell>
-                <StyledTableCell align='center'>
-                  Delivery Option
-                </StyledTableCell>
-                <StyledTableCell align='center'>Payment Method</StyledTableCell>
-                <StyledTableCell align='center'>Order Date</StyledTableCell>
-                <StyledTableCell align='center'>Cart Items</StyledTableCell>
-                <StyledTableCell align='center'>Address</StyledTableCell>
-                <StyledTableCell align='center'>Username</StyledTableCell>
-                <StyledTableCell align='center'>User ID</StyledTableCell>
-              </StyledTableRow>
-            </TableHead>
-            <TableBody>
+      <StyledTableContainer component={Paper}>
+        <StyledTable aria-label="simple table">
+          <TableHead>
+            <StyledTableRow>
+              <StyledTableCell align="center">ID</StyledTableCell>
+              <StyledTableCell align="center">Query ID</StyledTableCell>
+              <StyledTableCell align="center">Comment</StyledTableCell>
+              <StyledTableCell align="center">Total Price</StyledTableCell>
+              <StyledTableCell align="center">Delivery Option</StyledTableCell>
+              <StyledTableCell align="center">Payment Method</StyledTableCell>
+              <StyledTableCell align="center">Order Date</StyledTableCell>
+              <StyledTableCell align="center">Cart Items</StyledTableCell>
+              <StyledTableCell align="center">Address</StyledTableCell>
+              <StyledTableCell align="center">User Name</StyledTableCell>
+              <StyledTableCell align="center">User ID</StyledTableCell>
+            </StyledTableRow>
+          </TableHead>
+          <TableBody>
               {loading ? (
                 <TableRow>
-                  <TableCell align='center' colSpan={11}>
+                  <TableCell colSpan={11}>
                     <CircularProgress />
                   </TableCell>
                 </TableRow>
@@ -175,10 +140,10 @@ export function TableOrders() {
                 orders.map((order) => (
                   <TableRow
                     key={order.order_id}
-                    sx={{ backgroundColor: getRowColor(order.order_date) }}
+                    style={{ backgroundColor: getRowColor(order.order_date) }}
                   >
                     <TableCell>{order.order_id}</TableCell>
-                    {/* <TableCell>{order.queryId || '-'}</TableCell> */}
+                    <TableCell>{order.queryId || '-'}</TableCell>
                     <TableCell>{order.comment || '-'}</TableCell>
                     <TableCell>{order.totalPrice || '-'}</TableCell>
                     <TableCell>{order.optionDelivery || '-'}</TableCell>
@@ -242,7 +207,7 @@ export function TableOrders() {
                 </TableRow>
               )}
             </TableBody>
-          </StyledTable>
+            </StyledTable>
         </StyledTableContainer>
       </Box>
     </Box>
