fb6411e6c3c0f029c76a2de452c8b83bca599cca
yair-roshal <yair7705778@gmail.com>
2023-10-24 17:31:00 +0300

    delete 1 file and update 1 file

--- src/components/TableOrders.jsx
+++ src/components/TableOrders.jsx
@@ -101,7 +101,7 @@ export function TableOrders() {
     <Box sx={{ display: 'flex', justifyContent: 'center', width: '100%' }}>
       <Box p={2} sx={{ width: '100%' }}>
         <Typography variant='h4' align='center' mb={2}>
-          Orders Dashboard !!!
+          Orders Dashboard
         </Typography>
 
         <Box sx={{ marginTop: '20px', textAlign: 'center' }}>
