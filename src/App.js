4d19e039a696b117201932a0ec6ae30c367e8a10
yair-roshal <yair7705778@gmail.com>
2023-10-27 16:23:36 +0300

    build(deps): update package-lock.json, package.json, App.js and src/index.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,46 @@
-import './styles/App.scss'
-import { TableOrders } from './components/TableOrders'
+// import './styles/App.scss'
+// import { TableOrders } from './components/TableOrders'
 
-// const audio = new Audio('icq_sms_sound.mp3')
-// function playSound() {
-//   audio.play()
+// // const audio = new Audio('icq_sms_sound.mp3')
+// // function playSound() {
+// //   audio.play()
+// // }
+
+// function App() {
+//   return (
+//     <div className='App'>
+//       {/* <button onClick={playSound}>Play</button> */}
+
+//       <TableOrders />
+//     </div>
+//   )
 // }
 
-function App() {
-  return (
-    <div className='App'>
-      {/* <button onClick={playSound}>Play</button> */}
+// export default App
 
-      <TableOrders />
-    </div>
-  )
+
+import LoginButton from "./components/LoginButton";
+import LogoutButton from "./components/LogoutButton";
+import Profile from "./components/Profile";
+import { useAuth0 } from "@auth0/auth0-react";
+
+function App() {
+  const { isLoading, error } = useAuth0();
+
+  return (
+    <main className="column">
+      <h1>Auth0 Login</h1>
+      {error && <p>Authentication Error</p>}
+      {!error && isLoading && <p>Loading...</p>}
+      {!error && !isLoading && (
+        <>
+          <LoginButton />
+          <LogoutButton />
+          <Profile />
+        </>
+      )}
+    </main>
+  );
 }
 
-export default App
+export default App;
