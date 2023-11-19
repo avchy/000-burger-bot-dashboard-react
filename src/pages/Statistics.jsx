fd91a94a0f6dffcff9b11f5887d6d0ced5344290
yair-roshal <yair7705778@gmail.com>
2023-11-19 12:05:04 +0200

    update Home.jsx, Statistics.jsx and TableOrders.jsx

--- src/pages/Statistics.jsx
+++ src/pages/Statistics.jsx
@@ -1,30 +1,30 @@
-import React, { useState, useEffect } from 'react'
-import axios from 'axios'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import {
-  Box,
-  Table,
-  TableHead,
-  TableRow,
-  TableCell,
-  TableBody,
-  CircularProgress,
-  Paper,
-  Typography,
-} from '@mui/material'
+	Box,
+	Table,
+	TableHead,
+	TableRow,
+	TableCell,
+	TableBody,
+	CircularProgress,
+	Paper,
+	Typography,
+} from "@mui/material";
 
-import { useAuth0 } from '@auth0/auth0-react'
+import { useAuth0 } from "@auth0/auth0-react";
 
 export function Statistics() {
-  // const {   isAuthenticated } = useAuth0()
-	const isAuthenticated = true;
+	const { isAuthenticated } = useAuth0();
+	// const isAuthenticated = true;
 
-  return (
-    <>
-      {isAuthenticated && (
-        <Typography variant='h4' align='center' m={2}>
-          Statistics{' '}
-        </Typography>
-      )}
-    </>
-  )
+	return (
+		<>
+			{isAuthenticated && (
+				<Typography variant="h4" align="center" m={2}>
+					Statistics{" "}
+				</Typography>
+			)}
+		</>
+	);
 }
